8924e4d7ba1051338f2c2de7218c2acacda77800
Abhinav S <87767755+itzAbhinav@users.noreply.github.com>
2026-03-02 09:28:57 +1100

    Update set_matrix_zero.py

--- set_matrix_zero.py
+++ set_matrix_zero.py
@@ -3,17 +3,17 @@ class Solution:
     def SetMatrixZero(self,matrix):
             row = len(matrix)
             col = len(matrix[0])
-            row_arr = [0]*row
-            col_arr = [0]*col
+            row_arr = [0]*row             # row marker
+            col_arr = [0]*col             # column marker
             for i in range(0,row):
-                for j in range(0,col):
+                for j in range(0,col):       # Nested loop checks if matrix is equal to zero if yes then marks the row and col markers 1
                     if matrix[i][j] == 0:
                         row_arr[i] = 1
                         col_arr[j] = 1
           
             for i in range(0,row):
                 for j in range(0,col):
-                    if row_arr[i] == 1 or col_arr[j] == 1:
+                    if row_arr[i] == 1 or col_arr[j] == 1:   # Again reiterate the matrix if any row or col markers are 1 make the matrix elem 0
                         matrix[i][j] = 0
         
 
